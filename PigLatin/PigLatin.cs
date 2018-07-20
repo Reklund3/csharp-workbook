413dcf3e9ada0475c9c525739c829bd4dc7274cd
Robert Eklund <robert.eklund3@gmail.com>
2018-07-19 22:56:03 -0500

    add comments to code

--- PigLatin/PigLatin.cs
+++ PigLatin/PigLatin.cs
@@ -33,23 +33,33 @@ namespace PigLatin
             // leave this command at the end so your program does not close automatically
             Console.ReadLine();
         }
-        
+        // Function called when there is a vowel present in the
+        // current word. 
         public static void TranslateWord(ref string word, int position)
         {
             // your code goes here
             string punct = "";
             string temp = System.String.Empty;
+            // Condition to check if the first letter is a Y
+            // if the first letter is a y it is not treated as
+            // a vowel so we need to move the position off of 
+            // zero
             if (word[0] == 'y')
             {
                 position++;
             }
             string firstWord = word.Substring(0, position);
             string restWord = word.Substring(position);
+            // conditional check for punctuation, if there is punctuation
+            // the punctuation gets moved to the punct variable and removed
+            // from the end of the word for appending YAY or AY
             if (char.IsPunctuation(restWord[restWord.Length-1]))
             {
                 punct += restWord[restWord.Length-1];
                 restWord = restWord.Substring(0,restWord.Length-1);
             }
+            // if the first vowel is the first letter of the word
+            // then this condition will execute
             if (position == 0)
             {
                 word += "yay" + punct;
@@ -59,6 +69,10 @@ namespace PigLatin
                 word = restWord + firstWord + "ay" + punct;
             }
         }
+        // Function called when there are no vowels in a word
+        // this will only check for a punctuation at the end and
+        // if there is one move it to the end after adding the 
+        // ay to the end of the word
         public static void TranslateWord(ref string word)
         {
             string punct = "";
