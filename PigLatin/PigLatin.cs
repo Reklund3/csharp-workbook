09016cb126bff5c9d08a8e6b462a89db89935d09
Robert Eklund <robert.eklund3@gmail.com>
2018-07-19 22:45:44 -0500

    all condition working, follow up with comments

--- PigLatin/PigLatin.cs
+++ PigLatin/PigLatin.cs
@@ -16,15 +16,19 @@ namespace PigLatin
             
             for (int i = 0; i < pigWord.Length; i++)
             {
-                pigWord[i].ToLower();
+                pigWord[i] = pigWord[i].ToLower();
                 wordCount = pigWord[i].IndexOfAny(new char[] { 'a', 'e', 'i', 'o', 'u', 'y' });
-                if (wordCount != -1)
+                if (wordCount >= 0)
                 {
                     TranslateWord(ref pigWord[i], wordCount);
                     finalWord += pigWord[i] + ' ';
                 }
+                else 
+                {
+                    TranslateWord(ref pigWord[i]);
+                    finalWord += pigWord[i] + ' ';
+                }
             }
-            
             Console.WriteLine(finalWord.ToLower());
             // leave this command at the end so your program does not close automatically
             Console.ReadLine();
@@ -35,18 +39,16 @@ namespace PigLatin
             // your code goes here
             string punct = "";
             string temp = System.String.Empty;
+            if (word[0] == 'y')
+            {
+                position++;
+            }
             string firstWord = word.Substring(0, position);
             string restWord = word.Substring(position);
-
             if (char.IsPunctuation(restWord[restWord.Length-1]))
             {
                 punct += restWord[restWord.Length-1];
                 restWord = restWord.Substring(0,restWord.Length-1);
-            }
-            if (word[0] == 'y' || word[0] == 'Y') {
-                Console.WriteLine(position);
-                firstWord += 'y' + word.Substring(1,position);
-                position = 1;
             }
             if (position == 0)
             {
@@ -57,11 +59,21 @@ namespace PigLatin
                 word = restWord + firstWord + "ay" + punct;
             }
         }
+        public static void TranslateWord(ref string word)
+        {
+            string punct = "";
+            if (char.IsPunctuation(word[word.Length-1]))
+            {
+                punct += word[word.Length-1];
+                word = word.Substring(0,word.Length-1);
+            }
+            word = word + "ay" + punct;
+        }
     }
 }
 // Test Cases
 // child = ildchay     works
-// yellow = ellowyay   not
+// yellow = ellowyay   works
 // dye = yeday         works
-// bx = bxay           not
+// bx = bxay           works
 // eat = eatyay        works
