6f76a78bfc938bb97614060b2b7db43f12d8232c
Robert Eklund <robert.eklund3@gmail.com>
2018-07-21 20:57:32 -0500

    Upload FizzBuzz project

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -6,7 +6,29 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 100; ++i)
+            {
+                Console.WriteLine(checkFizzBuzz(i));
+            }
+        }
+        static string checkFizzBuzz(int x)
+        {
+            if (x % 15 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (x % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else if (x % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else
+            {
+                return x.ToString();
+            }
         }
     }
 }
