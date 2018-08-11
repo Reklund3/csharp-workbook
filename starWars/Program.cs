4a7db2e0fda45f2663a2942445de6e4b3c0f73f3
Robert Eklund <robert.eklund3@gmail.com>
2018-08-11 10:18:22 -0500

    Initial upload of Star Wars Project

---
+++ starWars/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace starWars
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Person leia = new Person("Leia", "Organa", "Rebel");
+			Person luke = new Person("Luke", "Skywalker","Rebel Jedi");
+            Person darth = new Person("Darth", "Vader", "Imperial");
+            Ship falcon = new Ship("Millennium Falcon", "Rebel", "Smuggling", 2);
+            Ship tie = new Ship("Tie Fighter", "Tie", "Fighter", 1);
+			Station spaceStationOne = new Station("Rebel Space Station","Rebel",5);
+			falcon.EnterShip(leia, 0);
+			falcon.EnterShip(luke,1);
+			tie.EnterShip(darth,0);
+			spaceStationOne.dockShip(falcon,0);
+			spaceStationOne.dockShip(tie,1);
+			Console.WriteLine(spaceStationOne.Ships);
+        }
+    }
+}
+
+class Person
+{
+	private string firstName;
+	private string lastName;
+	private string alliance;
+	public Person(string firstName, string lastName, string alliance)
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.alliance = alliance;
+	}
+
+	public string FullName
+	{
+		get
+		{
+			return this.firstName + " " + this.lastName;
+		}
+
+		set
+		{
+			string[] names = value.Split(' ');
+			this.firstName = names[0];
+			this.lastName = names[1];
+		}
+	}
+}
+class Ship
+{
+	private Person[] passengers;
+	public Ship(string ship, string alliance, string type, int size)
+	{
+        this.shipName = ship;
+		this.Type = type;
+		this.Alliance = alliance;
+		this.passengers = new Person[size];
+		
+	}
+    public string shipName { get; private set; }
+	public string Type { get; private set; }
+	public string Alliance { get; private set; }
+	public string Passengers
+	{
+		get
+		{
+			foreach (var person in this.passengers)
+			{
+				if (person != null)
+				{
+					Console.WriteLine(String.Format("{0}", person.FullName));
+				}
+			}
+
+			return "";
+		}
+	}
+	public void EnterShip(Person person, int seat)
+	{
+		this.passengers[seat] = person;
+	}
+	public void ExitShip(int seat)
+	{
+		this.passengers[seat] = null;
+	}
+}
+class Station
+{
+	private Ship[] ships;
+	public Station(string station, string alliance, int size)
+	{
+        this.stationName = station;
+		this.Alliance = alliance;
+		this.ships = new Ship[size];
+	}
+    public string stationName { get; set; }
+	public string Alliance { get; set; }
+	public string Ships
+	{
+		get
+		{
+			Console.WriteLine("The ships docked on the {0} are:", stationName);
+			foreach (var ship in ships)
+			{
+				if (ship != null)
+				{
+					Console.WriteLine(String.Format("{0}", ship.shipName));
+					Console.WriteLine("Crew:");
+					Console.WriteLine(ship.Passengers);
+				}
+			}
+
+			return "That's all the ships and their crew.";
+		}
+	}
+	public void dockShip(Ship ship, int landingPad)
+	{
+		this.ships[landingPad] = ship;
+	}
+	public void undock(int landingPad)
+	{
+		this.ships[landingPad] = null;
+	}
+}
