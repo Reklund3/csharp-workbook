794b5b9a1b04d70948d447d3fdef554a1f49063d
Robert Eklund <robert.eklund3@gmail.com>
2018-08-18 11:45:58 -0500

    Add comments to the code

--- TowersOfHanoi/TowersOfHanoi.cs
+++ TowersOfHanoi/TowersOfHanoi.cs
@@ -26,6 +26,8 @@ namespace TowersOfHanoi
                 game.movePiece();
             } while(!game.checkWin());
         }
+        // function to get the users input for how many blocks they would like to play with.
+        // throws an exception if the users input is not an int
         public static int getNumMoves()
         {
             int numBlocks;
@@ -44,6 +46,8 @@ namespace TowersOfHanoi
     class Game
     {
         Dictionary<string, Tower> towers = new Dictionary<string, Tower>();
+        // Game constructor takes in the number of blocks the user entered and
+        // creates the game with the number of blocks placed in Tower A stack
         public Game(int numBlocks)
         {
             towers["A"] = new Tower();
@@ -66,6 +70,9 @@ namespace TowersOfHanoi
                 Console.WriteLine();
             }
         }
+        // this method takes the tower key that was passed in from the
+        // towers Dictionary and pushes them into a temp stack so that
+        // they can be printed in the format of the game.
         public string displayBlocks(string towerName)
         {
             Stack<Block> display = new Stack<Block>();
@@ -79,6 +86,8 @@ namespace TowersOfHanoi
             }
             return "";
         }
+        // method check for if the tower the block is being moved to is empty or
+        // if the block being moved is smaller than the top block on that tower.
         public bool isLegal(Tower popOff, Tower pushOn)
         {
             if (pushOn.blockStack.Count == 0)
@@ -93,6 +102,8 @@ namespace TowersOfHanoi
             }
             return false;
         }
+        // This method takes in user inputs and stores them in the towerStart and towerEnd strings
+        // it them takes those inputs and passes them to the isLegal method to test the move 
         public void movePiece()
         {
             string towerStart = System.String.Empty;
@@ -119,6 +130,9 @@ namespace TowersOfHanoi
             }
             displayBoard();
         }
+        // method request the user to input from and to tower. It then
+        // checks to see if these entrys are valid. If the values are
+        // valid the reference is passed back. Else an exception is returned.
         public void getMove(ref string towerStart,ref string towerEnd)
         {
             string[] inputCheck = {"a", "b", "c"};
@@ -131,6 +145,8 @@ namespace TowersOfHanoi
                 throw new Exception("Tower entered was not a valid tower.");
             }
         }
+        // method to check if the player has won. It simply checks to see if towers A and B
+        // are empty. If they are the value returned is true.
         public bool checkWin()
         {
             if (towers["A"].blockStack.Count == 0 && towers["B"].blockStack.Count == 0)
@@ -140,10 +156,12 @@ namespace TowersOfHanoi
             return (towers["A"].blockStack.Count == 0 && towers["B"].blockStack.Count == 0);
         }
     }
+    // Tower class that holds the stack for each instance of a tower created.
     class Tower
     {
         public Stack<Block> blockStack = new Stack<Block>();
     }
+    // Block class that holds the blocks weight for each instance of a Block.
     class Block
     {
         public int weight {get; private set;}
